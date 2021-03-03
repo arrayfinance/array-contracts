e16979b1486076b3059d8ffab82c1d27fea4741e
Carl Farterson <carlfarterson@gmail.com>
2021-03-03 14:33:10 -0500

    Add: state variables, initialize(), buy() calculations

--- contracts/curve/Curve.sol
+++ contracts/curve/Curve.sol
@@ -10,15 +10,24 @@ interface I_BondingCurve{
 
 contract Curve {
 
-    uint256 public DEV_PCT = 3 * 10**17; // 30%
-    uint256 public PRECISION = 10**18;
+    
+    uint256 private DEV_PCT = 3 * 10**17; // 30%
+    uint256 private PRECISION = 10**18;
+
+    // Represents the 1MM DAI pledged
+    uint256 private STARTING_DAI_BALANCE = 1000000 * PRECISION;
+
+    // Starting supply of 10k ARRAY
+    uint256 private STARTING_ARRAY_MINTED = 10000 * PRECISION;
+
+    // Keeps track of supply sold for bonding curve
+    uint256 public virtualSupply;
+
+    // Used to calculate bonding curve slope
     uint32 public reserveRatio; // TODO
 
-    // To represent the 1MM DAI pledged
-    uint256 public virtualBalance = 1000000 * PRECISION;
-
-    // To represent the starting supply of 10k ARRAY
-    uint256 public virtualSupply = 10000 * PRECISION;
+    // Initial purchase of ARRAY from the CCO
+    bool private initialized;
 
     address public owner;
     address public devFund;
@@ -48,16 +57,44 @@ contract Curve {
         buy(virtualBalance);
     }
 
-    function buy(uint256 amountDai) {
+    function initialize(address to) public {
+        require(!initialized, "intialized");
+        require(msg.sender == owner, "!owner");
+
+        // Mint ARRAY to CCO
+        ARRAY.mint(to, STARTING_ARRAY_MINTED);
+
+        // Set virtual balance of supply to our 10k tokens
+        virtualSupply = STARTING_ARRAY_MINTED;
+
+        // Send 300k DAI to dev fund
+        DAI.transfer(devFund, 300000 * PRECISION);
+
+        initialized = true;
+    }
+
+
+
+    function buy(uint256 amountDai) public {
         require(amountDai > 0, "buy: cannot deposit 0 tokens");
         require(DAI.balanceOf(msg.sender) >= amountDai, "buy: cannot deposit more than user balance");
         
         require(DAI.transferFrom(msg.sender, address(this), amountDai), "buy: transferFrom failed");
 
-        // TODO
-        uint256 amountArray = CURVE.calculatePurchaseReturn(param);
+        uint256 amountArrayBuyer = CURVE.calculatePurchaseReturn(param);
 
-        emit Minted(msg.sender, amountArray, amountDai);
+        // TODO: calculations for buyer / dev team
+
+
+
+        // Update virtual balance of supply
+        virtualSupply = virtualSupply + 
+
+        
+        deposits[msg.sender] = deposits[msg.sender] + amountDai;
+        purchases[msg.sender] = purchases[msg.sender] + amountArrayBuyer;
+
+        emit Minted(msg.sender, amountArrayBuyer, amountDai);
     }
 
     function sell(uint256 amountArray) {
