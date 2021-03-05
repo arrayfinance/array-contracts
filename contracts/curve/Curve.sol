536fda5160f7f08a6b32ecd5c92ee0ab83d84019
Carl Farterson <carlfarterson@gmail.com>
2021-03-05 13:48:30 -0600

    Add: withdrawDevFunds() and updated buy() calculations

--- contracts/curve/Curve.sol
+++ contracts/curve/Curve.sol
@@ -15,6 +15,8 @@ contract Curve {
     uint256 private BUYER_PCT_ARRAY = 7 * 10**17; // 70%
     uint256 private PRECISION = 10**18;
 
+    uint256 public MAX_ARRAY_SUPPLY = 100000 * PRECISION;
+
     // Represents the 700k DAI spent for initial 10k tokens
     uint256 private STARTING_DAI_BALANCE = 700000 * PRECISION;
 
@@ -24,14 +26,14 @@ contract Curve {
     // Keeps track of DAI deposited
     uint256 public virtualBalance = STARTING_DAI_BALANCE;
 
+    // Keeps track of supply minted for bonding curve
+    uint256 public virtualSupply = STARTING_ARRAY_MINTED;
+
     // Keeps track of DAI for team
     uint256 public devFundDaiBalance;
 
     // Keeps track of ARRAY for team
     uint256 public devFundArrayBalance;
-
-    // Keeps track of supply minted for bonding curve
-    uint256 public virtualSupply = STARTING_ARRAY_MINTED;
 
     // Used to calculate bonding curve slope
     uint32 public reserveRatio; // TODO
@@ -41,6 +43,7 @@ contract Curve {
 
     address public owner;
     address public devFund;
+    address public gov;
     I_ERC20 public ARRAY;
     I_ERC20 public DAI;
     I_BondingCurve public CURVE;
@@ -85,36 +88,38 @@ contract Curve {
         require(DAI.balanceOf(msg.sender) >= amountDai, "buy: cannot deposit more than user balance");
         require(DAI.transferFrom(msg.sender, address(this), amountDai));
         
-        // 10% DAI allocated for dev team
-        uint256 amountDaiDevFund = amountDai * DEV_PCT_DAI / PRECISION;
-        devFundDaiBalance = devFundDaiBalance + amountDaiDevFund;
-        
-        // uint256 amountDaiCollateral = amountDai - amountDaiDevFund;
-
         uint256 amountArrayTotal = CURVE.calculatePurchaseReturn(
             virtualSupply,
             virtualBalance,
             reserveRatio,
             amountDai
         );
+        require(amountArrayTotal <= MAX_ARRAY_SUPPLY, "buy: amountArrayTotal > max supply");
 
-        // 20% ARRAY sent to dev team
-        // Since we're only buying ARRAY with 90% DAI deposited,
-        // Send 2/9 of array minted
-        uint256 amountArrayDevFund = amountArrayTotal * 2 / ; // TODO: remove hardcoded numbers
+        // Only track 90% of dai deposited, as 10% goes to team
+        uint256 amountDaiDeposited = amountDai * 9 / 10;
 
-        // Remaining 70% ARRAY sent to buyer, or 7/9 of ARRAY
-        uint256 amountArrayBuyer = amountArrayTotal - amountArrayDevFund;
+        // Only mint 90% of ARRAY
+        uint256 amountArrayMinted = amountArrayTotal * 9 / 10;  // TODO: hardcoding
+
+        // 70% of ARRAY minted sent to dev team
+        uint256 amountArrayBuyer = amountArrayTotal * 7 / 9;
+        
+        // Update dev fund balances
+        uint256 amountDaiDevFund = amountDai - amountDaiDeposited;
+        uint256 amountArrayDevFund = amountArrayMinted - amountArrayBuyer;
+        devFundDaiBalance = devFundDaiBalance + amountDaiDevFund;
+        devFundArrayBalance = devFundArrayBalance + amountArrayDevFund;
 
         // Update virtual balance and supply
-        virtualBalance = virtualBalance + amountDaiCollateral;
+        virtualBalance = virtualBalance + amountDaiDeposited;
         virtualSupply = virtualSupply + amountArrayTotal;
 
-        // TODO: only track 70% of deposit?
-        deposits[msg.sender] = deposits[msg.sender] + amountDai;
+        // Update mappings of deposits / purchases
+        deposits[msg.sender] = deposits[msg.sender] + amountDaiDeposited;
         purchases[msg.sender] = purchases[msg.sender] + amountArrayBuyer;
 
-        ARRAY.mint(devFund, amountArrayDevFund);
+        // Send array to buyer
         ARRAY.mint(msg.sender, amountArrayBuyer);
 
         emit Minted(msg.sender, amountArrayBuyer, amountDai);
@@ -124,9 +129,25 @@ contract Curve {
 
         if (max) {amountArray = ARRAY.balanceOf(msg.sender);}        
 
-
-
         emit Burned(msg.sender, amountArraySeller, amountDai);
+    }
+
+
+    function withdrawDevFunds(address _token, uint256 amount, bool max) returns (bool) {
+        require(msg.sender == gov, "withdrawDevFunds: !gov"); // TODO: gov address
+        require(_token == address(ARRAY) || _token == address(DAI), "withdrawDevFunds: not DAI or ARRAY");
+
+        if (_token == address(ARRAY)) {
+            require(amount <= devFundArrayBalance);
+            if (max) {amount = devFundArrayBalance;}
+            require(ARRAY.mint(devFund, amount));
+            devFundArrayBalance = devFundArrayBalance - amount;
+        } else { // _token == address(DAI)
+            require(amount <= devFundDaiBalance);
+            if (max) {amount = devFundDaiBalance;}
+            require(DAI.transfer(devFund, amount));
+            devFundDaiBalance = devFundDaiBalance - amount;
+        }
     }
 
 }
