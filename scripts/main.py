c75db4e77184e0d60fac0203a37104c78d172c40
Gismar <thegismar@gmail.com>
2021-03-15 23:20:48 -0300

    added vault (wip)

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,13 @@
-from brownie import ArrayFactory, ArrayProxyAdmin, ArrayProxyAdminTimelock, ArrayVault, accounts, address
+from brownie import *
 
-deployer = accounts[0]
-owner = accounts[1]
+# network.connect( 'ropsten' )
 
-af = deployer.deploy(ArrayFactory)
-apa = deployer.deploy(ArrayProxyAdmin)
-apat = deployer.deploy(ArrayProxyAdminTimelock(86400, [owner.address, deployer.address], address(0)) )
-av = deployer.deploy(ArrayVault)
+owner = accounts.load('ropsten_owner')
 
+af = owner.deploy( ArrayFactory, publish_source=True)
+apa = owner.deploy( ArrayProxyAdmin , publish_source=True)
+apat = owner.deploy( ArrayProxyAdminTimelock, 6400, [owner], [owner, ZERO_ADDRESS] , publish_source=True)
+av = owner.deploy( ArrayVault, publish_source=True )
+
+
+print('AF ' + af.address, 'APA ' + apa.address, 'APAT ' + apat.address, 'AV ' + av.address)
