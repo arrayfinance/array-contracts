e999d0f70b0c25823cbb41d31324e7815089f7ee
thegismar <thegismar@gmail.com>
2021-03-16 18:05:06 -0300

    Update IArrayToken.sol

--- contracts/interfaces/IArrayToken.sol
+++ contracts/interfaces/IArrayToken.sol
@@ -6,4 +6,5 @@ pragma solidity ^0.8.0;
 interface IArrayToken {
     function mint(address to, uint256 amount) external;
     function burn(address to, uint256 amount) external;
+    function totalSupply() external view returns (uint256);
 }
