4d68c632d8a4391b50ad5ca42493b76a7d5926cd
thegismar <thegismar@gmail.com>
2021-03-31 15:01:42 -0300

    added bancor formula tests

--- tests/test_curve.py
+++ tests/test_curve.py
@@ -1,5 +1,7 @@
 import pytest
-from brownie import Wei
+from rich.console import Console
+
+console = Console()
 
 
 @pytest.fixture( autouse=True )
@@ -13,17 +15,46 @@ def isolation(fn_isolation):
     pass
 
 
-# if we buy 1 ether worth of tokens, and then sell what we got, we should receive
-# approx 1 ether back.
-def test_bancor_formula(bancor, owner):
-    total_supply = Wei( '2 ether' )
-    poolBalance = Wei( '0.5 ether' )
+# how much does it cost to buy 10k tokens
+# starting with 0 supply and 0 collateral
+def test_bancor_formula():
+    # initially, when supply = 0 this is the formula
+
+    reserve = 1 / 3
+    slope = 1 / 1000000
+    amount = 10000
+
+    price = reserve * slope * (amount ** (1 / reserve))
+
+    console.print( f'\n\n Price with reserveRatio of {reserve} , slope of {slope} is {price} for the first {amount} '
+                   f'tokens\n' )
+
+    reserve = 1 / 3
+    slope = 3 / 1000000
+    amount = 10000
+
+    price = reserve * slope * (amount ** (1 / reserve))
+
+    console.print( f'\n Price with reserveRatio of {reserve} , slope of {slope} is {price} for the first {amount} '
+                   f'tokens\n\n' )
+
+
+# paying y TOKENS for x arrayTokens, then selling x arrayTokens should give us back y Tokens.
+def test_bancor_sanity(bancor):
+    total_supply = 100
+    poolBalance = 10
     cw = 1000000
-    buy = Wei( '1 ether' )
+
+    buy = 5
+
     purchase = bancor.calculatePurchaseReturn( total_supply, poolBalance, cw, buy )
     purchasev = purchase.return_value
+    console.print( f'[red] Purchased: {purchasev}' )
     total_supply = total_supply + purchasev
+
     poolBalance = poolBalance + buy
+
     sale = bancor.calculateSaleReturn( total_supply, poolBalance, cw, purchasev )
     salev = sale.return_value
-    assert salev / 1e18 == buy / 1e18
+    console.print( f'[green] Sale: {salev}' )
+    assert salev == buy
