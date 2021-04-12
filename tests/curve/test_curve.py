71c611a372d825575118f0b2b0124069e347bd00
thegismar <thegismar@gmail.com>
2021-04-12 14:36:23 -0300

    a little less broken -- static tests for curve contract pass -- deployer class created that unifies all contracts, tokens, prices, etc..

--- tests/curve/test_curve.py
+++ tests/curve/test_curve.py
@@ -3,68 +3,83 @@ import brownie
 from brownie import ZERO_ADDRESS
 
 
-@pytest.fixture( scope='function', autouse=True )
-def curve():
-    from scripts.setup import get_curve
-    yield get_curve()
+@pytest.fixture(scope='function', autouse=True)
+def deployer():
+    from scripts.deployer import Deployer
+    d = Deployer()
+    d.setup_curve()
+    yield d
 
 
-@pytest.fixture( autouse=True )
+@pytest.fixture(autouse=True)
 def isolation(fn_isolation):
     pass
 
 
-def test_revert_already_initialized_curve(curve, accounts):
+def test_revert_already_initialized_curve(deployer, accounts, isolation):
     initial_lp_tokens = 333_333 * 1e18
-    me = accounts[0]
-    with brownie.reverts( 'Initializable: contract is already initialized' ):
-        curve.initialize( initial_lp_tokens, {'from': me} )
+    accounts.default = deployer.me
+    with brownie.reverts('Initializable: contract is already initialized'):
+        deployer.crv.initialize(initial_lp_tokens)
 
 
 # verifies that the balance and supply is according to mxˆ2 with m = 1e-6
 # m = collateral / (CW * tokenSupply ^ (1 / CW))
-def test_correct_slope(curve, accounts):
-    collateral = curve.virtualBalance() / 1e18
-    supply = curve.virtualSupply() / 1e18
-    cw = curve.reserveRatio() / 1000000
+def test_correct_slope(deployer, isolation):
+    collateral = deployer.get_crv_balance() / 1e18
+    supply = deployer.get_crv_supply() / 1e18
+    cw = deployer.crv.reserveRatio() / 1000000
 
     m = collateral / (cw * supply ** (1 / cw))
-    m = round( m * 1e6 )
+    m = round(m * 1e6)
     assert m == 1
 
 
 # add, check for, and remove tokens to the virtual registry which inherits from oz's
 # enumerable set
-def test_add_remove_virtual_lp(tokens, curve, accounts):
-    me = accounts[0]
-    dai = tokens.dai
-    weth = tokens.weth
-    wbtc = tokens.wbtc
+def test_add_remove_virtual_lp(deployer, accounts, isolation):
+    accounts.default = deployer.me
+    dai = deployer.tokens.dai
+    weth = deployer.tokens.weth
+    wbtc = deployer.tokens.wbtc
 
-    assert curve.addTokenToVirtualLP( dai.address, {'from': me} )
-    assert curve.addTokenToVirtualLP( weth.address, {'from': me} )
-    assert curve.addTokenToVirtualLP( wbtc.address, {'from': me} )
+    assert deployer.crv.isTokenInVirtualLP(dai.address)
+    assert deployer.crv.isTokenInVirtualLP(weth.address)
+    assert deployer.crv.isTokenInVirtualLP(wbtc.address)
 
-    assert curve.isTokenInVirtualLP( dai.address )
-    assert curve.isTokenInVirtualLP( weth.address )
-    assert curve.isTokenInVirtualLP( wbtc.address )
-
-    assert curve.removeTokenFromVirtualLP( dai.address, {'from': me} )
-    assert curve.removeTokenFromVirtualLP( weth.address, {'from': me} )
-    assert curve.removeTokenFromVirtualLP( wbtc.address, {'from': me} )
+    assert deployer.crv.removeTokenFromVirtualLP(dai.address)
+    assert deployer.crv.removeTokenFromVirtualLP(weth.address)
+    assert deployer.crv.removeTokenFromVirtualLP(wbtc.address)
 
 
 # check if our pre-calc for array is rightk
-def test_correct_calc_estimation(curve, tokens, accounts, whales):
-    me = accounts[0]
-    dai = tokens.dai
-    whale = whales.dai
+def test_correct_calc_estimation(deployer, accounts, isolation):
+    accounts.default = deployer.me
+    dai = deployer.tokens.dai
     amount = 1000 * 1e18
 
-    dai.transfer( me, amount, {'from': whale} )
-    dai.approve( curve, amount, {'from': me} )
-
-    calc = curve.calculateArrayTokensGivenERC20Tokens( dai, amount )
-    tx = curve.buy( dai, amount, {'from': me} )
+    dai.approve(deployer.crv, amount)
+    calc = deployer.crv.calculateArrayTokensGivenERC20Tokens(dai, amount)
+    tx = deployer.crv.buy(dai, amount)
 
     assert calc == tx.return_value
+
+
+def test_buy(deployer, accounts, isolation):
+    accounts.default = deployer.me
+    for k, v in deployer.tokens.items():
+        prev = deployer.array.balanceOf(deployer.me)
+        m = min(v.balanceOf(deployer.me), (deployer.bpool.getBalance(v.address))/2 - 1000)
+        tx = deployer.crv.buy(v.address, m)
+        aft = deployer.array.balanceOf(deployer.me)
+        assert int(tx.return_value) == (aft - prev)
+
+
+def test_revert_no_balance(deployer, accounts, isolation):
+    accounts.default = accounts[0]
+    buy = 1000e18
+    coin = deployer.tokens['dai']
+    coin.transfer(ZERO_ADDRESS, coin.balanceOf(accounts[0]))
+    coin.approve(deployer.crv, buy)
+    with brownie.reverts("dev: user balance < amount"):
+        deployer.crv.buy(coin, buy, {'from' : accounts[0]})
